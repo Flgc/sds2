7f94197d4997743b83f5c7ddf4a97fde5ee98cd7
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-06 01:43:55 -0200

    GET /orders - implemented

--- BackEnd/src/main/java/net/fabiotecnico1/dsdeliver/repositories/OrderRepository.java
+++ BackEnd/src/main/java/net/fabiotecnico1/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package net.fabiotecnico1.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import net.fabiotecnico1.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrderWithProducts();
 }
--- BackEnd/src/main/java/net/fabiotecnico1/dsdeliver/services/OrderService.java
+++ BackEnd/src/main/java/net/fabiotecnico1/dsdeliver/services/OrderService.java
@@ -1,8 +1,25 @@
 package net.fabiotecnico1.dsdeliver.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import net.fabiotecnico1.dsdeliver.dto.OrderDTO;
+import net.fabiotecnico1.dsdeliver.entities.Order;
+import net.fabiotecnico1.dsdeliver.repositories.OrderRepository;
 
 @Service
 public class OrderService {
 
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
 }
