a42be22a020a292cc030b1261ed5b1421f2670bf
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-24 15:55:33 -0300

    Front-mobile - Home implemented

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import Header from './src/Header';
 import AppLoading from 'expo-app-loading';
+import Home from './src/Home';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,8 +18,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
+      <Header />
+      <Home />
+
     </View>
   );
 }
