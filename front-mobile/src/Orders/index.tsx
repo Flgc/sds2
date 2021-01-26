f8f958d20b4b448be60f7e4232a009e8c00c659d
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-26 19:56:54 -0300

    Front-mobile - Orders implemented layout

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from '../Header'
+import OrderCard from '../OrderCard'
 
 function Orders() {
 
    return (
       <>
          <Header />
-         <View >
-            <Text>Listagem de pedidos</Text>
+         <View style={styles.container}>
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
          </View>
       </>
    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+   container: {
+      paddingRight: '5%',
+      paddingLeft: '5%',
+   }
+});
 
 export default Orders;
