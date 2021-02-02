c7dcd869b75ecb2777aab06e85abeb6b0d267660
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-02 10:01:18 -0300

    Front-mobile - Order Details Start Navigation with google map gps mobile implemented

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, View, Text, Alert } from 'react-native';
+import { Link, useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, Text, Alert, Linking } from 'react-native';
 import Header from '../Header';
 import { Order } from '../types';
 import OrderCard from '../OrderCard';
@@ -34,14 +34,16 @@ function OrderDetails({ route }: Props) {
          })
    }
 
-
+   const handleStartNavigation = () => {
+      Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`);
+   }
 
    return (
       <>
          <Header />
          <View style={styles.container}>
             <OrderCard order={order} />
-            <RectButton style={styles.button}>
+            <RectButton style={styles.button} onPress={handleStartNavigation}>
                <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
             </RectButton>
             <RectButton style={styles.button} onPress={handleConfirmDelivery}>
